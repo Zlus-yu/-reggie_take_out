7944116d4031732b7e635ddba5a1ffda528bfbc4
Zlus <hello@itcast.cn>
2022-09-08 11:30:28 +0800

    提交项目

---
+++ src/main/java/com/zuochao/itheima/reggie/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.zuochao.itheima.reggie.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Zuo Chao
+ * @date 2022/9/8 11:29
+ */
+
+@RestController
+public class TestController {
+}
